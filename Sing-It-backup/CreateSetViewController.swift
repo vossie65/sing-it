53b7ce548163731fbb4d4505bfa9e62a21455c65
vossie65 <beratung@markus-voss.de>
2025-06-08 09:20:20 +0200

    Remove backup from git

--- Sing-It-backup/CreateSetViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class CreateSetViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-    }
-}
--- Sing-It-backup/PlaySetViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class PlaySetViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-    }
-}
